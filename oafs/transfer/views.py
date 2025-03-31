bb0d4846638a62d6f0cbdcd41e92a648b7fb1bc4
kiritharan <kiritharan@cmu.edu>
2025-03-31 12:30:12 -0400

    you can see some messages saved

--- oafs/transfer/views.py
+++ oafs/transfer/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import oaMessage
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def showMessages(request):
+	all_messages = oaMessage.objects.all()
+	return render(request, "allMessages.html",{'messages':all_messages})
