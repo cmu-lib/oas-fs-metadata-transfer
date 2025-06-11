ee32910d22f8727cd193df2a6c35b6aa1ca70733
kiritharan <kiritharan@cmu.edu>
2025-06-11 19:36:33 -0400

    updates  to templates

--- oafs/urls.py
+++ oafs/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('', views.oafsIndex, name='oafsIndex'),
     path('fsAttempts', views.fsAttempts, name='fsAttempts'),
     path('oaMessages', views.showMessages, name='showMessages'),
-    path('fsLicenses', views.showLicenses, name='showLifwfwefcenses'),
+    path('fsLicenses', views.showLicenses, name='showLicenses'),
     path('allErrors', views.allErrors, name='allErrors'),
     path('<int:key_id>/', views.retryOAConversion, name='retryOAConversion'),
 ]
