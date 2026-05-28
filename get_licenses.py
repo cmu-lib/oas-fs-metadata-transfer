f8688d3dc7ccf805dc00f0e271db9ac78720124f
kiritharan <kiritharan@cmu.edu>
2026-05-28 12:54:41 -0400

    cleanup top folder

--- get_licenses.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-params = {
-    'access_token': , #add access token here
-}
-
-our_licenses = requests.get('https://api.figsh.com/v2/account/licenses', params=params).json()
-
