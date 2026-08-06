302f3b6f25e36edd6b33b6e214798b7fd9c07900
kiritharan <kiritharan@cmu.edu>
2026-08-06 15:45:49 -0400

    updated model to remove status and note from indv message and attempt. so tracking is done through the other table of status messages. create token table to handle that via admin instead of manually editing files

--- transfer/models.py
+++ transfer/models.py
@@ -1,46 +1,53 @@
 from django.db import models
 from django import forms
 
-# i think this should be like a cursor. would it be better to have this saved as a file then?
-# the oa messages have failed on me a couple times when trying to make more than a couple requests
 
-#instead of doing NOTES here i think maybe have some other sort of table that will have "issues" as these can resolve.
-class oaMessageAttempts(models.Model): 
+# so you can modify this in the admin  
+class serviceCredentials(models.Model):
+    token = models.CharField(max_length=1024, help_text='token for whatever',null=True)
     attempted = models.DateTimeField(auto_now_add=True, blank=True)
-    total_messages = models.IntegerField(help_text="how many messages we gots", default=0)
-    last_offset = models.IntegerField(help_text="how far back", default=0)
+    name = models.CharField(max_length=1024, help_text='name for the service',null=False)
+    client_id = models.CharField(max_length=1024, help_text='client id for whatever',null=False)
+    client_secret = models.CharField(max_length=1024, help_text='client secret for whatever',null=False)
+    url = models.CharField(max_length=1024, help_text='url pointed at for the api calls. ie https://api.figshare.com/v2',null=True)
 
+class oaMessage(models.Model):  
+    oa_id = models.IntegerField(help_text="id of the messages",default=0)
+    title = models.CharField(max_length=1024, help_text='title of the article',null=True)
+    attempted = models.DateTimeField(auto_now_add=True, blank=True)
+    json = models.TextField(help_text="the json received",default=None,null=True)
+    
+    def __str__(self):
+        return self.title
+
+# specific to figshare
 class repoLicense(models.Model):
     value = models.CharField(max_length=1024, help_text='cc short code',null=True,unique=True)
     name = models.CharField(max_length=1024, help_text='longform',null=True,unique=True)
     url = models.CharField(max_length=1024, help_text='url for this license',null=True)
     def __str__(self):
         return self.name
-
-class oaMessage(models.Model):  
-    message_id = models.IntegerField(help_text="id of the messages",default=0)
-    message_title = models.CharField(max_length=1024, help_text='title of the article',null=True)
-    # statuses: ['transferred','ignore','error','ready','duplicate']
-    status = models.CharField(max_length=2048, help_text="status and maybe error message",null=True,default='ready')
-    note = models.TextField(help_text="what went wrong. what went write.",default="",null=True)
-    attempted = models.DateTimeField(auto_now_add=True, blank=True)
-    message_json = models.TextField(help_text="the json received",default=None,null=True)
-    
-    def __str__(self):
-        return self.message_title
-    
+        
 class fsAttempt(models.Model):  
-    oama_fk = models.ForeignKey('oaMessage', on_delete=models.CASCADE ,help_text='FK for oamessage') #oaMessageAttempt
+    oam_fk = models.ForeignKey('oaMessage', on_delete=models.CASCADE ,help_text='FK for oamessage') #oaMessageAttempt
     fs_id = models.IntegerField(help_text="filled in after successful upload to figshare",default=0)
-    # statuses: ['ready-for-push','ready-for-review','failed','published']
     doi = models.CharField(max_length=123, help_text="for comparing duplicate/revisions",null=False,default='0')
-    status = models.CharField(max_length=2048, help_text="status and maybe error message",null=True,default='ready-for-push')
     response = models.CharField(max_length=2048, help_text="response status from FS",null=True,default='')
     link = models.CharField(max_length=1024, help_text="link created for reference on fs",null=True)
     attempted= models.DateTimeField(auto_now_add=True, blank=True)
-    note = models.TextField(help_text="what went wrong. what went write.",default="",null=True)
-    fs_attempt_json = models.TextField(help_text="the json attempt",null=True)
+    json = models.TextField(help_text="the json attempt",null=True)
+    def __str__(self):
+            return self.oam_fk.title + " fs_id: " + str(self.fs_id)
 
+#general statuses/notes for both an oaMessage and fsAttempt if related.
+class messageStatus(models.Model):
+    note = models.TextField(help_text="what went wrong. what went right.",default="",null=True)
+    type = models.CharField(max_length=256, help_text="type of error.",default="",null=True)
+    status = models.CharField(max_length=256, help_text="type of error.",default="",null=True) # during the oa intake, during the conversion, during the push to figshare, etc.
+    oam_fk = models.ForeignKey('oaMessage',null=True, on_delete=models.CASCADE ,help_text='FK for oa ') #oaMessageAttempt
+    fs_fk = models.ForeignKey('fsAttempt', null=True, on_delete=models.CASCADE ,help_text='FK for fs') #oaMessageAttempt
+    # user_fk = models.ForeignKey('orcidUser', null=True, on_delete=models.CASCADE ,help_text='FK for users') #oaMessageAttempt
+    
 # class orcidUser(models.Model):
 #     f_name = models.CharField(max_length=512,help_text="whats yo name")
 #     l_name = models.CharField(max_length=512,help_text="who's yo daddy")
@@ -61,12 +68,3 @@ class fsAttempt(models.Model):
 #     f_name = models.CharField(max_length=512)
 #     l_name = models.CharField(max_length=512)
 #     full_name = models.CharField(max_length=512)
-
-# class errorMsg(models.Model):
-#     note = models.TextField(help_text="what went wrong. what went right.",default="",null=True)
-#     type = models.CharField(max_length=256, help_text="type of error.",default="",null=True)
-#     oama_fk = models.ForeignKey('oaMessage',null=True, on_delete=models.CASCADE ,help_text='FK for oa ') #oaMessageAttempt
-#     fs_fk = models.ForeignKey('fsAttempt', null=True, on_delete=models.CASCADE ,help_text='FK for fs') #oaMessageAttempt
-#     user_fk = models.ForeignKey('orcidUser', null=True, on_delete=models.CASCADE ,help_text='FK for users') #oaMessageAttempt
-#     level = models.IntegerField(default=0)
-#     
